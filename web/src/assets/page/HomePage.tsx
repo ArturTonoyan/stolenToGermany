46881d5c9f7a4c8bf406e07d9921e92a42a30fe7
Artur <artur.tonoyan.04@list.ru>
2024-07-30 15:29:34 +0300

    a

---
+++ web/src/assets/page/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./HomePage.module.scss";
+
+function HomePage() {
+  return <p>HomePage</p>;
+}
+
+export default HomePage;
---
+++ web/src/main.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+console.log(document.getElementById("root"));
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

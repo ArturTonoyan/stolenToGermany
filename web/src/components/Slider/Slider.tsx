72e1fcfd0e6e7b6c3fabc0e02312888a361988e7
Никита <100412266+Niks6455@users.noreply.github.com>
2024-08-03 23:49:46 +0300

    slider

--- web/src/components/Slider/Slider.tsx
+++ web/src/components/Slider/Slider.tsx
@@ -2,39 +2,45 @@ import React, { useState, useEffect } from "react";
 import styles from "./Slider.module.scss";
 
 const images = [
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
-  "./img/img1.svg",
+  "./img/imgSlider/1.png",
+  "./img/imgSlider/2.png",
+  "./img/imgSlider/3.png",
+  "./img/imgSlider/4.png",
+  "./img/imgSlider/5.png",
+  "./img/imgSlider/6.png",
+  "./img/imgSlider/7.png",
+  "./img/imgSlider/8.png",
+  "./img/imgSlider/9.png",
+  "./img/imgSlider/10.png",
+  "./img/imgSlider/11.png",
+  "./img/imgSlider/12.png",
 ];
 
 const Slider = () => {
   const [index, setIndex] = useState(12);
   const [isPaused, setIsPaused] = useState(false);
 
+  useEffect(() => { 
+   console.log("index", index);
+  }, [index]);
+
   useEffect(() => {
+    console.log("go")
     const interval = setInterval(() => {
       if (!isPaused) {
-        console.log(index);
-        if (index <= 0) {
-          setIndex(12);
-        } else {
+        console.log('indexISPause', index);
+        // if (index === 0) {
+        //   setIndex(12);
+        // } else {
           setIndex((prevIndex) => (prevIndex - 1) % images.length);
-        }
+        //}
       }
     }, 2000); // Rotate every 2 seconds
     return () => clearInterval(interval);
   }, [isPaused]);
 
   useEffect(() => {
+    console.log("paused")
     if (isPaused) {
       const timeout = setTimeout(() => {
         setIsPaused(false);
@@ -44,16 +50,24 @@ const Slider = () => {
   }, [isPaused]);
 
   useEffect(() => {
-    if (index === 3) {
+    const slider = document.getElementById("sliderBlock");
+    if (slider) {
+      slider.style.transition = "transform 0.5s ease";  // Add smooth transition
+      slider.style.transform = `rotate(${index * 30}deg)`;  // Smoothly rotate by 30 degree
+    }
+    if (index === 0) {
       // Assuming the left-center image is at index 3
-      setIsPaused(true);
+      // setIsPaused(true);
+      setIndex(12)
     }
   }, [index]);
-
+  
   return (
-    <div className={styles.Slider}>
+    <div className={styles.Slider} id="sliderBlock">
       {images.map((image, i) => {
-        const angle = (360 / 12) * i + 90;
+        const angle = (360 / 12) * i + 180;
+        console.log('angle', angle)
+        console.log('index * 30', ( index * 30) + 180)
         const isActive = index === i;
         return (
           <img
@@ -71,8 +85,10 @@ const Slider = () => {
                 50 + 40 * Math.sin(((2 * Math.PI) / images.length) * i)
               }%`,
               transform: `rotate(${angle}deg)`,
-              width: isActive ? "200px" : "160px", // Enlarge the active image
-              height: isActive ? "200px" : "160px",
+              
+              width:  angle - 30  === (180- index * 30) + 150 ? "200px" : "160px",
+              height:  angle - 30 === (180-index * 30) + 150 ? "200px" : "160px",
+              
             }}
           />
         );

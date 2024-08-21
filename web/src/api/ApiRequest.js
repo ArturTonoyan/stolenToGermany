015bd37c7f74d79e6e3e7801f90596308102f34f
Никита <100412266+Niks6455@users.noreply.github.com>
2024-08-21 19:08:35 +0300

    Update ApiRequest.js

--- web/src/api/ApiRequest.js
+++ web/src/api/ApiRequest.js
@@ -18,57 +18,57 @@ export const apiOstarbaiters = async () => {
   }
 };
 
-export const CreateEducator = async (data) => {
-  try {
-    const response = await http.post(`${server}/educator/`, data);
-    return response;
-  } catch (error) {
-    console.error("Error:", error);
-    return error;
-    //throw error;
-  }
-};
+// export const CreateEducator = async (data) => {
+//   try {
+//     const response = await http.post(`${server}/educator/`, data);
+//     return response;
+//   } catch (error) {
+//     console.error("Error:", error);
+//     return error;
+//     //throw error;
+//   }
+// };
 
-//! запрос на добавление преподавателя к нагрузке
-export const addEducatorWorkload = async (data) => {
-  console.log("Добавление преподавателя ", data);
-  try {
-    const response = await http.patch(`${server}/workload/faculty`, data);
-    console.log("response ", response);
-    return response.data;
-  } catch (error) {
-    console.error("Error:", error);
-    //throw error;
-  }
-};
+// //! запрос на добавление преподавателя к нагрузке
+// export const addEducatorWorkload = async (data) => {
+//   console.log("Добавление преподавателя ", data);
+//   try {
+//     const response = await http.patch(`${server}/workload/faculty`, data);
+//     console.log("response ", response);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error:", error);
+//     //throw error;
+//   }
+// };
 
-//! запрос на удаление нагрузки
-export const deleteWorkload = async (data) => {
-  console.log("Нагрузки удалены ", data);
-  try {
-    const response = await http.delete(
-      `${server}/workload/deleteSeveralWorkloads`,
-      { data: data }
-    );
-    console.log("response ", response);
-    return response.data;
-  } catch (error) {
-    console.error("Error:", error);
-    //throw error;
-  }
-};
+// //! запрос на удаление нагрузки
+// export const deleteWorkload = async (data) => {
+//   console.log("Нагрузки удалены ", data);
+//   try {
+//     const response = await http.delete(
+//       `${server}/workload/deleteSeveralWorkloads`,
+//       { data: data }
+//     );
+//     console.log("response ", response);
+//     return response.data;
+//   } catch (error) {
+//     console.error("Error:", error);
+//     //throw error;
+//   }
+// };
 
-//! запрос на изменени данных в админке
-export const apiAdminUpdata = async (data) => {
-  console.log("изменение данных админке ", data);
-  try {
-    const response = await http.put(`${server}/user/${data.id}/update`, {
-      [data.key]: data.value,
-    });
-    console.log("response ", response);
-    return response;
-  } catch (error) {
-    console.error("Error:", error);
-    //throw error;
-  }
-};
+// //! запрос на изменени данных в админке
+// export const apiAdminUpdata = async (data) => {
+//   console.log("изменение данных админке ", data);
+//   try {
+//     const response = await http.put(`${server}/user/${data.id}/update`, {
+//       [data.key]: data.value,
+//     });
+//     console.log("response ", response);
+//     return response;
+//   } catch (error) {
+//     console.error("Error:", error);
+//     //throw error;
+//   }
+// };

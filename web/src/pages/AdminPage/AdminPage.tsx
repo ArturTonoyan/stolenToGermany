85f1807bbd3f99be5ccdc0bcbda2f67d846c3294
Artur <artur.tonoyan.04@list.ru>
2024-09-03 19:09:46 +0300

    1

--- web/src/pages/AdminPage/AdminPage.tsx
+++ web/src/pages/AdminPage/AdminPage.tsx
@@ -4,31 +4,31 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { apiCheckAuthorization } from "../../api/ApiRequest";
 
 function AdminPage(props: any) {
-  const [action, setAction] = useState<boolean>(false);
-  const navigate = useNavigate();
-  useEffect(() => {
-    apiCheckAuthorization().then((res) => {
-      if (res?.status === 200) {
-        console.log("авторизован", res);
-        setAction(true);
-      } else {
-        setAction(false);
-      }
-    });
-  });
-  useEffect(() => {
-    console.log("admin", props.loc);
-    if (props.loc === "/AdminPage/AdminPageAuth" && action) {
-      navigate("/AdminPage/AdminPanelModule");
-    }
-    if (
-      props.loc.includes("AdminPage") &&
-      !action &&
-      props.loc !== "/AdminPage/AdminPageAuth"
-    ) {
-      navigate("/");
-    }
-  }, [props.loc, action]);
+  // const [action, setAction] = useState<boolean>(false);
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   apiCheckAuthorization().then((res) => {
+  //     if (res?.status === 200) {
+  //       console.log("авторизован", res);
+  //       setAction(true);
+  //     } else {
+  //       setAction(false);
+  //     }
+  //   });
+  // });
+  // useEffect(() => {
+  //   console.log("admin", props.loc);
+  //   if (props.loc === "/AdminPage/AdminPageAuth" && action) {
+  //     navigate("/AdminPage/AdminPanelModule");
+  //   }
+  //   if (
+  //     props.loc.includes("AdminPage") &&
+  //     !action &&
+  //     props.loc !== "/AdminPage/AdminPageAuth"
+  //   ) {
+  //     navigate("/");
+  //   }
+  // }, [props.loc, action]);
 
   return (
     <div className={styles.AdminPage}>

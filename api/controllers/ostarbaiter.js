01aa50f0079a9b9130a5ee4cd89a29b8855db637
IgorTrofimenko <tro@sfedu.ru>
2024-09-05 12:01:40 +0300

    fix create and update

--- api/controllers/ostarbaiter.js
+++ api/controllers/ostarbaiter.js
@@ -5,6 +5,12 @@ import Ostarbeiter from "../models/index.js";
 import {AppErrorInvalid, AppErrorMissing, AppErrorNotExist} from "../utils/error.js";
 import axios from "axios";
 import { Op } from "sequelize";
+const validateLetter  = (str) => /^[А-ЯЁ]+$/i.test(str)
+const validateDeparture  = (str) => /^[А-ЯЁ.,-]+$/i.test(str)
+const validateNumeric = (str) => /^(18[4-9][0-9]|19[0-3][0-9]|194[0-4])+$/i.test(str)
+const validateLetterNumeric = (str) => /^[А-Яа-яЁё0-9.,;:!?\"'()\-—\s]+$/i.test(str)
+
+
 
 export default {
   async get({ query }, res) {
@@ -95,8 +101,18 @@ export default {
     },
     res
   ) {
-
-    console.log(dateDeparture)
+    if (!surname || !validateLetter(surname)) throw new AppErrorMissing("surname");
+    if (!date) throw new AppErrorMissing("date");
+    if(date && !validateNumeric(date))  throw new AppErrorInvalid('date')
+    if(name && !validateLetter(name)) throw  new AppErrorInvalid('name')
+    if(patronymic && !validateLetter(patronymic)) throw  new AppErrorInvalid('patronymic')
+    if(profession && !validateLetter(profession)) throw new AppErrorInvalid('profession')
+    if(dateDeparture && !validateNumeric(dateDeparture) ) throw new AppErrorInvalid('dateDeparture')
+    if(localityWork && !validateDeparture(localityWork)) throw new AppErrorInvalid('localityWork')
+    if(localityDeparture && !validateDeparture(localityDeparture)) throw new AppErrorInvalid('localityDeparture')
+    if(addressAfterReturning && !validateLetterNumeric(addressAfterReturning)) throw new AppErrorInvalid('addressAfterReturning')
+    if(infoOfRepatriation && !validateLetterNumeric(infoOfRepatriation)) throw new AppErrorInvalid('infoOfRepatriation')
+    if(infoOfDeath && !validateLetterNumeric(infoOfDeath)) throw new AppErrorInvalid('infoOfDeath')
 
     const ostarbaiter = await Ostarbeiter.findByPk(ostarbaiterId);
     if (!ostarbaiter) throw new AppErrorNotExist("ostarbaiter");
@@ -137,8 +153,20 @@ export default {
     },
     res
   ) {
-    if (!surname) throw new AppErrorMissing("surname");
+
+    if (!surname || !validateLetter(surname)) throw new AppErrorMissing("surname");
     if (!date) throw new AppErrorMissing("date");
+    if(date && !validateNumeric(date))  throw new AppErrorInvalid('date')
+    if(name && !validateLetter(name)) throw  new AppErrorInvalid('name')
+    if(patronymic && !validateLetter(patronymic)) throw  new AppErrorInvalid('patronymic')
+    if(profession && !validateLetter(profession)) throw new AppErrorInvalid('profession')
+    if(dateDeparture && !validateNumeric(dateDeparture) ) throw new AppErrorInvalid('dateDeparture')
+    if(localityWork && !validateDeparture(localityWork)) throw new AppErrorInvalid('localityWork')
+    if(localityDeparture && !validateDeparture(localityDeparture)) throw new AppErrorInvalid('localityDeparture')
+    if(addressAfterReturning && !validateLetterNumeric(addressAfterReturning)) throw new AppErrorInvalid('addressAfterReturning')
+    if(infoOfRepatriation && !validateLetterNumeric(infoOfRepatriation)) throw new AppErrorInvalid('infoOfRepatriation')
+    if(infoOfDeath && !validateLetterNumeric(infoOfDeath)) throw new AppErrorInvalid('infoOfDeath')
+
     await Ostarbeiter.create({
       surname: surname,
       name: name,
@@ -200,7 +228,7 @@ export default {
 
     const { count, rows } = await Ostarbeiter.findAndCountAll({
       where: {
-        localityWork: { [Op.like]: `%${filters.localityWork}%` },
+        localityWork: { [Op.iLike]: `%${filters.localityWork}%` },
       },
     });
 

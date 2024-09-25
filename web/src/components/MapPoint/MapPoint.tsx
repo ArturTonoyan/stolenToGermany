b32d5107211f070430cc9c79831784bc379ef2d6
Artur <artur.tonoyan.04@list.ru>
2024-09-25 22:42:34 +0300

    1

--- web/src/components/MapPoint/MapPoint.tsx
+++ web/src/components/MapPoint/MapPoint.tsx
@@ -3,6 +3,8 @@ import styles from "./MapPoint.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { setModalOpen, setSelectedPoint } from "../../store/basic/camps.slice";
 import { RootState } from "../../store/store";
+import { useEffect, useRef } from "react";
+// import { RootState } from "../../store/store";
 
 function MapPoint(props: any) {
   const dispatch = useDispatch();
@@ -24,6 +26,25 @@ function MapPoint(props: any) {
       </div>`;
   };
 
+  const plRef = useRef<HTMLDivElement>(null);
+  // useEffect(() => {
+  //   // Найдите все элементы с классом "ymaps-2-1-79-image"
+  //   const elements = document.querySelectorAll(".ymaps-2-1-79-image");
+  //   // Перебирайте каждый элемент и добавьте число к классу
+  //   console.log("elements", elements);
+  //   elements.forEach((element, index) => {
+  //     element.innerHTML = `<span class="number" style="
+  //         width: 100%;
+  //         heigth: 100%;
+  //         color: white;
+  //         display: flex;
+  //         justify-content: center;
+  //         align-items: center;
+  //         font-size: 16px;
+  //         font-weight: bold;
+  //     ">${index + 1}</span>`;
+  //   });
+  // }, [plRef, props.item]);
   const svgData = `<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="16" cy="16" r="16" fill="#F8F7FF"/>
     <circle cx="16" cy="16" r="12" fill="#5B6A99"/>
@@ -39,6 +60,7 @@ function MapPoint(props: any) {
       <Placemark
         ref={plRef}
         onClick={funClickPoint}
+        // onBalloonClose={handleBalloonClose}
         modules={["geoObject.addon.balloon"]}
         defaultGeometry={props.item?.point}
         properties={{
@@ -46,8 +68,8 @@ function MapPoint(props: any) {
         }}
         options={{
           iconLayout: "default#image",
-          iconImageHref: "./img/point.svg",
-          // iconImageHref: iconImageHref,
+          // iconImageHref: "./img/point.svg",
+          iconImageHref: iconImageHref,
         }}
       ></Placemark>
     </div>
